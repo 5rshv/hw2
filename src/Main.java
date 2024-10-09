07b5a77f785f582297bcabe8fbe138b5e36bc979
5rshv <serishev.alesha@gmail.com>
2024-10-09 19:21:47 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-                var dog = 8.0;
+                var d = 8.0;
                 var cat = 3.6;
                 var paper = 763789;
                 var friend = 19;
